7b22c58e57cd88083c9f9a3b24151b398c8eca26
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2023-12-06 13:03:03 +0000

    Beginning
    Imported an asset bundle for prototyping.
    
    Working on properly positioning the main camera.
    
    Built the 'base'

---
+++ AI_Project/Assets/DD/Scripts/DD_PlayerInputManager.cs
@@ -0,0 +1,126 @@
+// ----------------------------------------------------------------------
+// --------------------  AI: Input Manager
+// -------------------- David Dorrington, UoB Games, 2023
+// ---------------------------------------------------------------------
+
+using UnityEngine;
+
+public class DD_PlayerInputManager : MonoBehaviour
+{
+    // ---------------------------------------------------------------------
+    // Camera Control
+    public GameObject gameCamera;
+    public float camMoveSpeed = 10f;
+    public float selectionBorderSize = 50;
+
+    // Mouse Select
+    public Vector2 leftClickPosition = new(-1, -1);
+    public Vector2 rightClickPosition = new(-1, -1);
+    public Vector2 leftDownPosition = new(-1, -1);
+    public Vector2 leftUpPosition = new(-1, -1);
+
+    public bool mouseLUP = false;
+    public bool mouseLDown = false;
+    public bool mouseRDown = false;
+
+    public int lastKeyPressed = 0;
+
+
+    // ---------------------------------------------------------------------
+    void Update()
+    {
+        GetPlayerMouseClick();
+        CameraControl();
+    }//-----
+
+
+
+
+    // ---------------------------------------------------------------------
+    private void CameraControl()
+    {
+        // Detect input and update camera postion
+        float newCamX = gameCamera.transform.position.x + Input.GetAxis("Horizontal") * camMoveSpeed * Time.deltaTime;
+        float newCamZ = gameCamera.transform.position.z + Input.GetAxis("Vertical") * camMoveSpeed * Time.deltaTime;
+        float newCamY = gameCamera.transform.position.y;
+
+
+        if (Input.mousePosition.y > 0 && Input.mousePosition.y < selectionBorderSize) newCamZ = gameCamera.transform.position.z - camMoveSpeed * Time.deltaTime; 
+        if (Input.mousePosition.x > 0 && Input.mousePosition.x < selectionBorderSize) newCamX = gameCamera.transform.position.x - camMoveSpeed * Time.deltaTime; ;
+        if (Input.mousePosition.y < Screen.height && Input.mousePosition.y > Screen.height - selectionBorderSize) newCamZ = gameCamera.transform.position.z + camMoveSpeed * Time.deltaTime; ;
+        if (Input.mousePosition.x < Screen.width &&  Input.mousePosition.x > Screen.width - selectionBorderSize) newCamX = gameCamera.transform.position.x + camMoveSpeed * Time.deltaTime; ;
+
+        gameCamera.transform.position = new(newCamX, newCamY, newCamZ);
+
+        // Zoom by adjusting Field of view with mouse wheel
+        if (Input.mouseScrollDelta.y < 0 && gameCamera.GetComponent<Camera>().fieldOfView < 90)
+            gameCamera.GetComponent<Camera>().fieldOfView++;
+
+        if (Input.mouseScrollDelta.y > 0 && gameCamera.GetComponent<Camera>().fieldOfView > 10)
+            gameCamera.GetComponent<Camera>().fieldOfView--;
+    }//---
+
+    // ================================================================================
+
+    private void GetPlayerMouseClick()
+    {
+
+    
+
+        //if the left or right mouse button clicked
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            // Create Ray cast source and target 
+            Ray rayCamToMousePostion = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // cast Ray 100m and store data in _rh_hit)
+            if (Physics.Raycast(rayCamToMousePostion, out RaycastHit rayHit, 100F))
+            {
+                // Set Position LM Clicked id on the gameBoard 
+                if (Mathf.Round(rayHit.point.x) > 0 && Mathf.Round(rayHit.point.x) < 100
+                    && Mathf.Round(rayHit.point.z) > 0 && Mathf.Round(rayHit.point.z) < 100)
+                {
+                    if (Input.GetMouseButtonDown(0)) // Left Button                   
+                        leftClickPosition = new((int)rayHit.point.x, (int)rayHit.point.z);
+
+                    if (Input.GetMouseButtonDown(1)) // Right Button            
+                    {
+                        rightClickPosition = new((int)rayHit.point.x, (int)rayHit.point.z);
+                        mouseRDown = true;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0)) // Left Mouse still held Down
+        {
+            mouseLUP = false;
+            mouseLDown = true;
+            // Create Ray cast source and target 
+            Ray rayCamToMousePostion = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // cast Ray 100m and store data in _rh_hit)
+            if (Physics.Raycast(rayCamToMousePostion, out RaycastHit rayHit, 100F))
+            {
+                // Set Position LM Clicked id on the gameBoard 
+                if (Mathf.Round(rayHit.point.x) > 0 && Mathf.Round(rayHit.point.x) < 100
+                    && Mathf.Round(rayHit.point.z) > 0 && Mathf.Round(rayHit.point.z) < 100)
+                    leftDownPosition = new((int)rayHit.point.x, (int)rayHit.point.z);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseLUP = true;
+            mouseLDown = false;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            mouseRDown = false;
+        }
+
+    }//------
+
+
+}//==========
---
+++ AI_Project/Assets/DD/Scripts/GameObjects/DD_UnitPlayerControl.cs
@@ -0,0 +1,233 @@
+// ----------------------------------------------------------------------
+// --------------------  AI: Unit Player Control
+// -------------------- David Dorrington, UoB Games, 2023
+// ---------------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_UnitPlayerControl : MonoBehaviour
+{
+    // ---------------------------------------------------------------------
+    private DD_GameManager gameManager;
+    private DD_Unit unitScript = null;
+    public Vector3 playerSetTarget = Vector3.zero;
+    public DD_Team parentTeam;
+
+    // squad
+    [Header("Squad Stuff")]
+    public int squadID = -1;
+    public int squadPattern;
+    public bool isSquadLeader;
+
+    private GameObject highlight;
+    public bool isSelected = false;
+
+    // check if squad active
+    // Check unit is part of squad
+
+    // ---------------------------------------------------------------------
+    private void Start()
+    {
+        // The game manager will be use to access the game board
+        gameManager = GameObject.Find("GameManager").GetComponent<DD_GameManager>();
+        parentTeam = GetComponentInParent<DD_Team>();
+        // Unit Control script
+        unitScript = GetComponent<DD_Unit>();
+        CreateHighlight();
+    }//-----
+
+
+    // ---------------------------------------------------------------------
+    private void FixedUpdate()
+    {
+        UpdateHighlight();
+        StateManagerPlayerControl();
+        ManageSquad();
+    }//-----
+
+
+
+    // ---------------------------------------------------------------------
+    public GameObject squadLeader;
+
+    private void ManageSquad()
+    {
+        if (unitScript.isMoving) return;
+
+        if (!parentTeam.squadActive)
+        {
+            isSquadLeader = false;
+        }
+        else
+        {// check pos in squad
+            squadID = -1;
+            int index = 1;
+            foreach (GameObject unit in parentTeam.squad)
+            {
+                if (index == 1) squadLeader = unit;
+
+                if (unit == this.gameObject)
+                {
+                    squadID = index;
+                }
+                index++;
+            }
+        }
+
+        if (squadID == 1)
+            isSquadLeader = true;
+        else
+            isSquadLeader = false;
+
+        if (squadID < 1) squadLeader = null;
+    }//-----
+
+
+    public bool useArrayFormation = false;
+
+    // ---------------------------------------------------------------------
+    private void SetSquadPosition()
+    {
+        int xOffset = 0, zOffset = 0;
+        if (useArrayFormation)
+        {
+            int leaderOffsetX = 0, leaderOffsetZ = 0;
+
+            for (int row = 0; row < parentTeam.squadShape.GetLength(0); row++)
+            {
+                for (int col = 0; col < parentTeam.squadShape.GetLength(1); col++)
+                {
+                    if (parentTeam.squadShape[row, col] == 1)
+                    {
+                        leaderOffsetX = col;
+                        leaderOffsetZ = row;
+                    }
+                    if (parentTeam.squadShape[row, col] == squadID)
+                    {
+                        xOffset = col; zOffset = row;
+                    }
+                }
+            }
+            xOffset -= leaderOffsetX; zOffset -= leaderOffsetZ;
+        }
+        else// use Algorithm 
+        {
+            xOffset = 0;
+            zOffset = squadID ;
+        }
+
+        unitScript.targetPosition = new(squadLeader.transform.position.x + xOffset, 0, +squadLeader.transform.position.z + zOffset);
+
+        // Check if enemy is close
+        if (Vector3.Distance(unitScript.nearestEnemyPosition, unitScript.currentPosition) < unitScript.enemyChaseRange)
+        {
+            unitScript.AttackEnemy();
+        }
+
+        // Move to squad Postion
+        if (Vector3.Distance(unitScript.targetPosition, unitScript.currentPosition) > unitScript.stopRange)
+            unitScript.ChaseDirect(false);
+
+        // Avoid Obstacle
+        if (unitScript.obstacleAhead)
+        {
+            unitScript.unitState = States.roam;
+            unitScript.obstacleAhead = false;
+        }
+
+    }//-----
+
+
+    // ---------------------------------------------------------------------
+    private void StateManagerPlayerControl()
+    {
+        if (unitScript.isPlayerControlled == false) return;
+        if (unitScript.isMoving) return;
+        if (unitScript.isDepositing) return;
+
+
+        if (squadID > 1) // part of squad but not leader
+        {
+            SetSquadPosition();
+            return;
+        }
+
+
+        if (isSelected)
+        {
+            unitScript.WaypointMoveToTarget(gameManager.playerSetTargetPos);
+        }
+        else // Wander, gather and fight
+        {
+            unitScript.unitState = States.wander;
+
+            // Check Resource in Range 
+            if (Vector3.Distance(unitScript.currentPosition, unitScript.nearestResourcePosition) < unitScript.resourceRange)
+            {
+                // Check Resource is in Line of sight before moving towards it
+                if (gameManager.ai.CheckTargetInLineOfSight(unitScript.currentPosition, unitScript.nearestResourcePosition))
+                {
+                    unitScript.targetPosition = unitScript.nearestResourcePosition;
+                    unitScript.unitState = States.chase;
+                }
+
+            }
+
+            if (Vector3.Distance(unitScript.currentPosition, unitScript.nearestResourcePosition) < unitScript.stopRange)
+                unitScript.unitState = States.idle;
+
+            // wander if out of range 
+            if (Vector3.Distance(unitScript.currentPosition, unitScript.nearestResourcePosition) > unitScript.resourceRange)
+                unitScript.unitState = States.wander;
+
+            // Harvest Resource if close
+            if (Vector3.Distance(unitScript.nearestResourcePosition, unitScript.currentPosition) <= unitScript.stopRange)
+                unitScript.unitState = States.harvest;
+
+            // Depoit Resource when full
+            if (unitScript.resourceCarrying > unitScript.resourceLimit - 0.1F)
+                unitScript.unitState = States.deposit;
+
+            // Check if enemy is close
+            if (Vector3.Distance(unitScript.nearestEnemyPosition, unitScript.currentPosition) < unitScript.enemyChaseRange)
+            {
+                unitScript.AttackEnemy();
+            }
+        }
+
+        // is the path blocked and not wandering
+        if (unitScript.unitState != States.wander)
+        {
+            if (unitScript.obstacleAhead)
+            {
+                unitScript.unitState = States.roam;
+                unitScript.obstacleAhead = false;
+            }
+        }
+    }//-----
+
+    // ---------------------------------------------------------------------
+    private void UpdateHighlight()
+    {
+        if (isSelected)
+            highlight.SetActive(true);
+        else
+            highlight.SetActive(false);
+    }//-----
+
+
+    // ---------------------------------------------------------------------
+    private void CreateHighlight()
+    {
+        highlight = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        highlight.transform.position = new(transform.position.x, 0.7f, transform.position.z);
+        highlight.transform.localScale = new(0.8f, 0.01f, 0.8f);
+        highlight.transform.SetParent(transform);
+        highlight.SetActive(false);
+    }//----
+
+
+
+}//===============================
+
